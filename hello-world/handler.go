0dbcc537b764e9415cdf9d36b1aca2746f873559
Matias Pan <matias.pan26@gmail.com>
2019-02-22 18:11:18 -0300

    Initial commit
    Signed-off-by: Matias Pan <matias.pan26@gmail.com>

---
+++ hello-world/handler.go
@@ -0,0 +1,15 @@
+package function
+
+import (
+	"net/http"
+
+	handler "github.com/openfaas-incubator/go-function-sdk"
+)
+
+// Handle is a function that always returns:  Hello world!
+func Handle(req handler.Request) (handler.Response, error) {
+	return handler.Response{
+		Body:       []byte("Hello world!"),
+		StatusCode: http.StatusOK,
+	}, nil
+}
