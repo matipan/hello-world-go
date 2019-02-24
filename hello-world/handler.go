28cac95379b41998e628b9176c7f97d167684996
Matias Pan <matias.pan26@gmail.com>
2019-02-24 23:45:43 +0000

    Update function file for ofc e2e test
    Signed-off-by: Matias Pan <matias.pan26@gmail.com>

--- hello-world/handler.go
+++ hello-world/handler.go
@@ -9,7 +9,7 @@ import (
 // Handle is a function that always returns:  Hello world!
 func Handle(req handler.Request) (handler.Response, error) {
 	return handler.Response{
-		Body:       []byte("2019-02-24 23:43:02.835495391 +0000 UTC m=+6.727764129"),
+		Body:       []byte("2019-02-24 23:45:43.282988691 +0000 UTC m=+8.332446951"),
 		StatusCode: http.StatusOK,
 	}, nil
 }
