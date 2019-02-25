2332d97cf3ffa49a444e3ef5b6cf327bccdde36c
Matias Pan <matias.pan26@gmail.com>
2019-02-25 19:10:20 -0300

    Update function file for ofc e2e test
    Signed-off-by: Matias Pan <matias.pan26@gmail.com>

--- hello-world/handler.go
+++ hello-world/handler.go
@@ -1,1 +1,15 @@
-2019-02-25 22:04:36.249585279 +0000 UTC m=+7.124000669
+package function
+
+import (
+	"net/http"
+
+	handler "github.com/openfaas-incubator/go-function-sdk"
+)
+
+// Handle is a function that always returns:  Hello world!
+func Handle(req handler.Request) (handler.Response, error) {
+	return handler.Response{
+		Body:       []byte("2019-02-25 22:10:20.66471465 +0000 UTC m=+27.460919558"),
+		StatusCode: http.StatusOK,
+	}, nil
+}
