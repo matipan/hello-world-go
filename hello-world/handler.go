14aa00126b2ce1d12a6c0660b645fa1e82c23721
OpenFaaS e2e test <notreal@openfaas.com>
2019-03-15 13:39:59 +0000

    Update function file for ofc e2e test

--- hello-world/handler.go
+++ hello-world/handler.go
@@ -9,7 +9,7 @@ import (
 // Handle is a function that always returns:  Hello world!
 func Handle(req handler.Request) (handler.Response, error) {
 	return handler.Response{
-		Body:       []byte("2019-03-15 13:36:52.827242759 +0000 UTC m=+6.467537919"),
+		Body:       []byte("2019-03-15 13:39:59.453440255 +0000 UTC m=+31.095107699"),
 		StatusCode: http.StatusOK,
 	}, nil
 }
